287b964922387acd0086ad1d1b3e94276fd9d86f
Yuriy Sokolov <79514840030@ya.ru>
2022-10-25 23:06:52 +0600

    переделал открытие кластеров и изначальное заполнение

--- projects/ymaps/index.js
+++ projects/ymaps/index.js
@@ -13,36 +13,44 @@ function mapInit() {
       center: [55.76, 37.65],
       zoom: 9,
     });
+
     const clusterer = new ymaps.Clusterer({
       groupByCoordinates: true,
       clusterDisableClickZoom: true,
+      clusterOpenBalloonOnClick: false,
     });
+    clusterer.options.set('hasBallon', false);
+    clusterer.events.add('click', function (e) {
+      e.preventDefault();
+      const coordinates = e.get('target').getGeoObjects()[0].geometry._coordinates;
+      openBalloon(coordinates);
+    });
+
     myMap.geoObjects.add(clusterer);
 
     function showOnMap() {
       for (const review of reviews) {
-        addPlacemark(review.coordinates, createHtml(review.coordinates));
+        addPlacemark(review.coordinates);
       }
     }
     showOnMap();
 
     myMap.events.add('click', function (e) {
+      e.preventDefault();
       const coordinates = e.get('coords');
       openBalloon(coordinates);
     });
 
     async function openBalloon(coords) {
       await myMap.balloon.open(coords, createHtml(coords));
-
-      // const form = document.querySelector('form')
-      // console.log(form)
-      // form.addEventListener('submit', (e) => {
-      //   e.preventDefault()
-      //   const coordinates = form.getAttribute('data-coordinates');
-      //   addNewReview(form, coordinates);
-      //   addPlacemark(coordinates);
-      //   closeBaloon();
-      // });
+      const form = document.querySelector('form');
+      form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const coordinates = form.getAttribute('data-coordinates');
+        addNewReview(form, coordinates);
+        addPlacemark(coordinates);
+        closeBaloon();
+      });
     }
 
     function createHtml(coordinates) {
@@ -67,25 +75,14 @@ function mapInit() {
       return html;
     }
 
-    document.addEventListener('click', (e) => {
-      if (e.target.classList.contains('review__submit')) {
-        const form = e.target.closest('form');
-        const coordinates = form.getAttribute('data-coordinates');
-        form.addEventListener('submit', (e) => e.preventDefault());
-        addNewReview(form, coordinates);
-        addPlacemark(coordinates);
-        closeBaloon();
-      }
-    });
-
     function addPlacemark(coordinates) {
-      const myPlacemark = new ymaps.Placemark(coordinates.split(','), {
-        balloonContentBody: createHtml(coordinates),
+      const myPlacemark = new ymaps.Placemark(coordinates.split(','));
+
+      myPlacemark.events.add('click', (e) => {
+        e.stopPropagation();
+        openBalloon(myPlacemark.geometry._coordinates);
       });
 
-      myPlacemark.events.add('click', () => {
-        myPlacemark.properties.set('balloonContent', createHtml(coordinates));
-      });
       placemarks.push(myPlacemark);
       myMap.geoObjects.add(myPlacemark);
       clusterer.add(placemarks);
@@ -108,6 +105,7 @@ function mapInit() {
 
     function fillReviews(coordinates) {
       let reviesHtml = '';
+      coordinates = coordinates.join(',');
       for (const review of reviews) {
         if (coordinates === review.coordinates) {
           reviesHtml += `<p><b>${review.name}</b>[${review.place}] <br> ${review.review}</p>`;
