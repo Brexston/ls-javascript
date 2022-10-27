92dbc2d083b0fecc9ace3350f12098afc3aa95dc
Yuriy Sokolov <79514840030@ya.ru>
2022-10-28 01:22:27 +0600

    накидал верстку для чата и серверную часть

---
+++ projects/chat/server/server.js
@@ -0,0 +1,38 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 4000 }, () => {
+  console.log('Server started');
+});
+
+const clients = new Set();
+
+wss.on('connection', (wsClient) => {
+  clients.add(wsClient);
+
+  wsClient.on('message', (message) => {
+    const request = JSON.parse(message.toString);
+    broadcast(request);
+  });
+  wsClient.on('close', () => {
+    clients.delete(wsClient);
+  });
+});
+
+function broadcast(params) {
+  let response;
+
+  clients.forEach((client) => {
+    switch (params.event) {
+      case 'login':
+        response = { type: 'login', payload: {} };
+        break;
+      case 'message':
+        response = { type: 'message', payload: {} };
+        break;
+      default:
+        response = { type: 'logout', payload: {} };
+        break;
+    }
+    client.send(JSON.stringify(response));
+  });
+}
